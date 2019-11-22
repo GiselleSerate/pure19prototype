9024047b80d7511049892e4086091bd14523f3f6
GiselleSerate <giselle.serate@gmail.com>
2019-11-21 19:42:42 -0800

    Write code for ports, start code for running procs

--- prototype.py
+++ prototype.py
@@ -13,6 +13,7 @@ USERNAME = 'root'
 
 
 
+# TODO: this is a lot of sequential scripting. How do we feel about a class?
 if __name__ == "__main__":
     client = SSHClient()
     # Explore ~/.ssh/ for keys
@@ -30,14 +31,38 @@ if __name__ == "__main__":
             version = line.split('=')[1].split('"')[1]
     print(f"FROM {operating_sys}:{version}")
 
-
     # GET PACKAGES
+    # packages = [] # lol we aren't actually using this line are we
     if operating_sys == 'centos':
         stdin, stdout, stderr = client.exec_command('yum list installed')
         for line in stdout:
-            print(line.strip())
+            # Skip not-data
+            if not re.match(r'.+ +.+ +@[A-Za-z]+', line):
+                continue
+            pkg, ver, src = line.split()
+            print(f"pkg:{pkg} ver:{ver} src:{src}")
     else:
         raise Exception(f"Unsupported operating system {operating_sys}: we don't know what package manager you're using.")
+
+    # GET PORTS
+    # TODO: How do I know I'm not getting my own port that I'm using for ssh? Is it just literally port 22?
+    # What if we try to run this on something that uses 22?
+    stdin, stdout, stderr = client.exec_command('netstat -lntp')
+    for line in stdout:
+        # Skip header
+        if (re.match(r'Proto Recv-Q Send-Q Local', line)
+            or re.match(r'Active Internet', line)):
+            continue
+        proto, recv_q, send_q, local, foreign, state, pid = line.split()
+        pid, progname = pid.split('/')
+        print(f"proto:{proto} recv_q:{recv_q} send_q:{send_q} local:{local} foreign:{foreign} state:{state} pid:{pid} progname:{progname}")
+
+    # GET RUNNING PROCS
+    # stdin, stdout, stderr = client.exec_command('ps -ao pid,cmd | grep -v \\ps -ao pid,cmd\\')
+    # stdin, stdout, stderr = client.exec_command('ps -ao pid,cmd')
+    stdin, stdout, stderr = client.exec_command('ps -aux')
+    for line in stdout:
+        print(line.rstrip())
 
     # Make sure you kill the connection when you're done
     client.close()

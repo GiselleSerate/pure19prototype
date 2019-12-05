2622abff8a6de133ddd16d7f7b5a420371ba4d33
GiselleSerate <giselle.serate@gmail.com>
2019-12-04 21:31:00 -0800

    Write test to reveal missing packages

--- test/test_prototype.py
+++ test/test_prototype.py
@@ -2,7 +2,7 @@
 
 import logging
 
-# from src.prototype import SystemAnalyzer
+from src.prototype import SystemAnalyzer
 
 
 
@@ -14,17 +14,33 @@ LOG_LEVEL = 'INFO'
 
 
 
-# def test_basic_build():
-#     with SystemAnalyzer(hostname=HOSTNAME, port=PORT, username=USERNAME) as kowalski:
-#         kowalski.get_os()
-#         kowalski.get_packages()
-#         kowalski.filter_packages()
-#         kowalski.get_ports()
-#         kowalski.get_procs()
-#         kowalski.dockerize()
-#         try:
-#             kowalski.docker_client.images.build(path=kowalski.dir)
-#         except Exception as e:
-#             logging.error("Failed at building image")
-#             logging.error(e)
-#             raise e
+def test_integration_basic_build():
+    with SystemAnalyzer(hostname=HOSTNAME, port=PORT, username=USERNAME) as kowalski:
+        kowalski.get_os()
+        kowalski.get_packages()
+        kowalski.dockerize()
+
+        # Now that we have a Dockerfile, build and check the packages are there
+        image, _ = kowalski.docker_client.images.build(tag='pytest', path=kowalski.dir)
+        container = kowalski.docker_client.containers.run(image=image.id, command="yum list installed", detach=True)
+        # Block until the command's done, then check its output.
+        container.wait()
+        output = container.logs()
+        output = output.decode()
+        logging.error(output)
+
+        there = 0
+        total = 0
+        missing = []
+        for package in kowalski.packages.keys():
+            if package in output:
+                there += 1
+            else:
+                missing.append(package)
+            total += 1
+
+        # Fully aware this isn't how you should be writing a test, thanks
+        if there < total:
+            logging.error(f"{there}/{total}")
+            logging.error(f"The following packages were missing: {missing}")
+        assert there == total

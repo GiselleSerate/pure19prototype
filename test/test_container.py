2ce5106c9a4e5b8ed6786f0a503fa0cf6ba9ec94
GiselleSerate <giselle.serate@gmail.com>
2020-03-31 20:55:40 -0700

    Lint tests

--- test/test_container.py
+++ test/test_container.py
@@ -1,11 +1,16 @@
+'''
+Provides tests to verify that a container can be converted to another container.
+
+container_tester is a helper function with the actual function of the tests.
+test_*_container are the actual tests, which specify behaviors for the container_tester function.
+'''
+
 import logging
 import os
 import re
 import tempfile
 
 import docker
-from paramiko import SSHClient
-from paramiko.ssh_exception import NoValidConnectionsError
 
 from src.prototype import GeneralAnalyzer, SystemAnalyzer
 
@@ -29,7 +34,10 @@ def container_tester(name, operating_sys, port, username, expected, install_str)
     # Set up sshable container
     logging.info(f"Setting up base {name} container . . .")
     docker_client = docker.from_env()
-    base_image, _ = docker_client.images.build(tag=f'test_{name}_base_img', path=os.path.join(os.getcwd(), "containers", operating_sys))
+    base_image, _ = docker_client.images.build(tag=f'test_{name}_base_img',
+                                               path=os.path.join(os.getcwd(),
+                                                                 "containers",
+                                                                 operating_sys))
 
     try:
         base_container = docker_client.containers.run(base_image.id, detach=True, ports={22: port})
@@ -41,21 +49,25 @@ def container_tester(name, operating_sys, port, username, expected, install_str)
 
         # Analyze container
         logging.info(f"Analyzing {name} container . . .")
-        with GeneralAnalyzer(hostname=HOSTNAME, port=port, username=username, auto_add=True) as kowalski:
-            kowalski.analyzer.get_packages()
-            kowalski.analyzer.filter_packages()
-            # All packages should be able to be installed (i.e. can find all packages with proper versions).
-            all_correct = kowalski.analyzer.verify_packages(mode=SystemAnalyzer.Mode.dry)
+        with GeneralAnalyzer(hostname=HOSTNAME, port=port, username=username, auto_add=True) as gen:
+            gen.analyzer.get_packages()
+            logging.info(gen.analyzer.all_packages)
+            gen.analyzer.filter_packages()
+            # All packages should be able to be installed (i.e. can find all packages with proper
+            # versions).
+            all_correct = gen.analyzer.verify_packages(mode=SystemAnalyzer.Mode.dry)
             assert all_correct
 
             # Create the Dockerfile
             tempdir = tempfile.mkdtemp()
-            kowalski.analyzer.dockerize(tempdir)
+            gen.analyzer.dockerize(tempdir)
 
             # Make a container from it
             logging.info(f"Verifying {name} container . . .")
-            verify_image, _ = docker_client.images.build(tag=f'test_{name}_verify_img', path=tempdir)
-            verify_container = docker_client.containers.run(verify_image.id, detach=True, command=kowalski.analyzer.LIST_INSTALLED)
+            verify_image, _ = docker_client.images.build(tag=f'test_{name}_verify_img',
+                                                         path=tempdir)
+            verify_container = docker_client.containers.run(verify_image.id, detach=True,
+                                                            command=gen.analyzer.LIST_INSTALLED)
             verify_container.wait()
             for pkg in expected:
                 logging.info(f"Checking package {pkg} . . .")
@@ -67,21 +79,25 @@ def container_tester(name, operating_sys, port, username, expected, install_str)
         verify_container.remove(force=True)
         logging.info(f"Cleaned up {operating_sys} successfully.")
 
-# TODO: Something's weird with Ubuntu right now. I don't know why.
-# def test_basic_ubuntu_container():
-#     '''
-#     Test that basic sshable ubuntu container can be put through the prototype
-#     '''
-#     expected = ['openssh-server']
-#     container_tester(name='basic_ubuntu', operating_sys='ubuntu', port=PORT, username=USERNAME, expected=expected, install_str='apt-get install -y')
+# TODO: Something's weird with Ubuntu right now. I think libsqlite-0 was a default package that now
+# cannot be installed for some reason. This is bizarre.
+def test_basic_ubuntu_container():
+    '''
+    Test that basic sshable ubuntu container can be put through the prototype
+    '''
+    expected = ['openssh-server']
+    container_tester(name='basic_ubuntu', operating_sys='ubuntu', port=PORT, username=USERNAME,
+                     expected=expected, install_str='apt-get install -y')
 
 
-# def test_assorted_ubuntu_container():
-#     '''
-#     Test that ubuntu container with a selection of assorted packages can be put through the prototype
-#     '''
-#     expected = ['openssh-server', 'rolldice', 'ghc', 'git']
-#     container_tester(name='assorted_ubuntu', operating_sys='ubuntu', port=PORT, username=USERNAME, expected=expected, install_str='apt-get install -y')
+def test_assorted_ubuntu_container():
+    '''
+    Test that ubuntu container with a selection of assorted packages can be put through the
+    prototype
+    '''
+    expected = ['openssh-server', 'rolldice', 'ghc', 'git']
+    container_tester(name='assorted_ubuntu', operating_sys='ubuntu', port=PORT, username=USERNAME,
+                     expected=expected, install_str='apt-get install -y')
 
 
 def test_basic_centos_container():
@@ -89,14 +105,15 @@ def test_basic_centos_container():
     Test that basic sshable centos container can be put through the prototype
     '''
     expected = ['openssh-server', 'openssh-clients']
-    container_tester(name='basic_centos', operating_sys='centos', port=PORT, username=USERNAME, expected=expected, install_str='yum install -y')
+    container_tester(name='basic_centos', operating_sys='centos', port=PORT, username=USERNAME,
+                     expected=expected, install_str='yum install -y')
 
 
 def test_assorted_centos_container():
     '''
-    Test that centos container with a selection of assorted packages can be put through the prototype
+    Test that centos container with a selection of assorted packages can be put through the
+    prototype
     '''
     expected = ['openssh-server', 'openssh-clients', 'gdb', 'valgrind', 'wireshark']
-    container_tester(name='assorted_centos', operating_sys='centos', port=PORT, username=USERNAME, expected=expected, install_str='yum install -y')
-
-
+    container_tester(name='assorted_centos', operating_sys='centos', port=PORT, username=USERNAME,
+                     expected=expected, install_str='yum install -y')
--- test/test_prototype.py
+++ test/test_prototype.py
@@ -1,24 +1,29 @@
-import logging
-import tempfile
+'''
+Provides local tests for Centos and Ubuntu. Don't run these in CI.
+'''
 
 from src.prototype import GeneralAnalyzer, SystemAnalyzer
 
+
+
 # Constants (which we can move into a config file later)
 HOSTNAME = '127.0.0.1'
+
+
 
 def test_local_basic_build_centos():
     '''
     Test that all packages from our CentOS system can be installed
     (first try with the specified version for all; failing that, try latest).
     '''
-    PORT = 2222
-    USERNAME = 'root'
-    with GeneralAnalyzer(hostname=HOSTNAME, port=PORT, username=USERNAME) as kowalski:
+    port = 2222
+    username = 'root'
+    with GeneralAnalyzer(hostname=HOSTNAME, port=port, username=username) as kowalski:
         kowalski.analyzer.get_packages()
         kowalski.analyzer.filter_packages()
         no_wrong_versions = kowalski.analyzer.verify_packages(mode=SystemAnalyzer.Mode.unversion)
         if no_wrong_versions:
-            # Wish I had a meaningful assert for you, but if you hit this case you just pass the test.
+            # Wish I had a meaningful assert for you, but if you hit this case you just pass.
             assert no_wrong_versions
         else:
             # Some wrong versions. Try to compensate.
@@ -30,19 +35,16 @@ def test_local_basic_build_ubuntu():
     Test that all packages from our ubuntu system can be installed
     (first try with the specified version for all; failing that, try latest).
     '''
-    PORT = 3333
-    USERNAME = 'squirrel'
-    with GeneralAnalyzer(hostname=HOSTNAME, port=PORT, username=USERNAME) as kowalski:
+    port = 3333
+    username = 'squirrel'
+    with GeneralAnalyzer(hostname=HOSTNAME, port=port, username=username) as kowalski:
         kowalski.analyzer.get_packages()
         kowalski.analyzer.filter_packages()
         no_wrong_versions = kowalski.analyzer.verify_packages(mode=SystemAnalyzer.Mode.unversion)
         if no_wrong_versions:
-            # Wish I had a meaningful assert for you, but if you hit this case you just pass the test.
+            # Wish I had a meaningful assert for you, but if you hit this case you just pass.
             assert no_wrong_versions
         else:
             # Some wrong versions. Try to compensate.
             no_wrong_packages = kowalski.analyzer.verify_packages(mode=SystemAnalyzer.Mode.dry)
             assert no_wrong_packages
-
-
-
